ef967fddb48bde3b5f22e46bff30fee8a2247f86
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2023-11-05 21:45:01 +0200

    button (interactive - multiline) 25%

--- packages/components/button/src/Button.tsx
+++ packages/components/button/src/Button.tsx
@@ -1,4 +1,5 @@
 // import {UseButtonProps, useButton} from "./use-button";
+import { useRef } from "react";
 import "./Button.scss";
 
 // export interface ButtonProps extends UseButtonProps {}
@@ -34,10 +35,37 @@ import "./Button.scss";
 
 type buttonPropsT = {
 	children: React.ReactNode;
+	interactive?: boolean;
+	multiline?: boolean;
 };
 
-function Button({ children }: buttonPropsT) {
-	return <button type="button">{children}</button>;
+function Button({ children, interactive = false, multiline = false }: buttonPropsT) {
+	const ButtonRef = useRef<HTMLButtonElement>(null);
+
+	// ele.getBoundingClientRect()
+
+	return (
+		<button
+			ref={ButtonRef}
+			type="button"
+			className={`Moon-Rock_Button ${interactive ? "Button--interactive" : ""} ${multiline ? "Button--multiline" : ""}`}
+		>
+			{children}
+		</button>
+	);
 }
 
 export default Button;
+
+/*
+	? button roll:
+	- min size 30px (touch-based interactions)
+	- touch targets should be at least 48 x 48
+	- min margin 8px
+	-  36px high, min width 88px
+*/
+
+// TODO: make a fun that send a warring if the size is less than 37px
+// TODO: make a hook that use this js cool size thing to get any element size
+// TODO: when taking the button size make accept px, rem, em and if he does not paht a value make the defalt px and after all of that make the vinal value with rem
+// TODO: make the multi line subort
--- packages/components/button/stories/button.stories.tsx
+++ packages/components/button/stories/button.stories.tsx
@@ -1,9 +1,22 @@
-// import React from "react";
-import Button from "../src/Button";
+import React from "react";
+
+import Button from "../src/Button.tsx";
 
 export default {
 	title: "button",
 	component: Button,
 };
 
-export const Test = () => <button>tes</button>;
+export const Normal_Button = () => <Button>Button</Button>;
+
+export const Interactive_Button = () => <Button interactive>Button</Button>;
+
+export const Multiline_Button = () => {
+	return (
+		<div style={{ width: "3rem" }}>
+			<Button multiline>
+				this button have some much text that it needs more than on line to fit
+			</Button>
+		</div>
+	);
+};
